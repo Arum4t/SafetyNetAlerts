2b78e8d1ec486ca32ffe61c44b974282b5340b5e
Quentin <quentin.schnurr@gmail.com>
2022-04-27 16:30:02 +0200

    Add log request and catch exception

--- src/main/java/com/safetynetalerts/webapp/repository/DataLoaderRepository.java
+++ src/main/java/com/safetynetalerts/webapp/repository/DataLoaderRepository.java
@@ -2,6 +2,9 @@ package com.safetynetalerts.webapp.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynetalerts.webapp.model.Response;
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +13,8 @@ import java.io.IOException;
 
 @Repository
 public class DataLoaderRepository implements IDataLoaderRepository {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataLoaderRepository.class);
 
         private ObjectMapper objectMapper;
         private Response response;
@@ -20,8 +25,17 @@ public class DataLoaderRepository implements IDataLoaderRepository {
         }
 
        @Override
-        public Response getResponse() throws IOException {
-            response = objectMapper.readValue(new File("src/main/resources/json_list.json"), Response.class);
+        public Response getResponse(){
+            if(response != null){
+                return response;
+            }
+            try{
+                response = objectMapper.readValue(new File("src/main/resources/json_list.json"), Response.class);
+                logger.debug("Json correctly mapped!");
+            } catch (IOException e){
+                logger.error("Error while JSON mapping!", e);
+                throw new RuntimeException(e);
+            }
             return response;
-        }
+       }
 }
